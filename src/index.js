565540c7d88b932399987d34582c80e085ff3b6b
Tanvi Dua <tanvidua5774@gmail.com>
2025-04-20 14:44:49 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
@@ -10,6 +11,7 @@ dotenv.config();
 const PORT= process.env.PORT||3001;
 const app= express();
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
