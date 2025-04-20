5656452426e0808c046fd7eabed6bdff605cff7a
Tanvi Dua <tanvidua5774@gmail.com>
2025-04-20 14:27:10 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+dotenv.config();
+
+const PORT= process.env.PORT||3001;
+const app= express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth",authRoutes)
+app.use("/api/books",bookRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on ${PORT}`);
+    connectDB();
+})
